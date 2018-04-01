5d327e5564954f7adb5c410673276b0f3fe16f84
Yash Patel <ypatel@princeton.edu>
2018-04-01 02:12:12 -0400

    Added directory structure and main barebones

--- app.py
+++ app.py
@@ -4,8 +4,16 @@ __description__ = Main application file to be run
 __name__ = app.py
 """
 
+import cv2
+
+from generate import generate_3d
+from detect_board import get_corners
+
 def main():
-    pass
+    frame = 0
+    while True:
+        frame_img = cv2.imread("stream/{}.jpg".format(frame))
+        board_corners = get_corners(frame_img)
 
 if __name__ == "__main__":
     main()
---
+++ stream/README.md
@@ -0,0 +1,2 @@
+# Stream
+Where all images are stored from the video stream (from the Raspberry Pi camera) -- should be empty 
