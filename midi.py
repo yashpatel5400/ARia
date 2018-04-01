bee5f92e761d737b08fbbaafc36c69d3e2234f59
Yash Patel <ypatel@princeton.edu>
2018-04-01 05:05:49 -0400

    Converted MIDI notes to letters

--- midi.py
+++ midi.py
@@ -1,6 +1,16 @@
+"""
+__authors__     = Yash, Peter, Jeff, Robert
+__description__ = Reads MIDI files and returns list of notes to be played
+__name__ = midi.py
+"""
+
 from mido import MidiFile
 
 import sys
+
+# converts from MIDI note to letter note 
+# from (https://www.midikits.net/midi_analyser/midi_note_numbers_for_octaves.htm)
+NOTES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
 
 def get_note_sequence(file):
     mid = MidiFile(file)
@@ -16,7 +26,8 @@ def get_note_sequence(file):
         elif msg.type == 'note_off':
             time += msg.time
             index = current_notes[msg.note]
-            note_sequence[index] = (msg.note, note_sequence[index][1], time)
+            note_sequence[index] = (NOTES[msg.note % len(NOTES)], 
+                note_sequence[index][1], time)
             del current_notes[msg.note]
 
     return note_sequence
